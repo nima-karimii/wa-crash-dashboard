ee018b0b55c19a3a178e16fe381b7ca66ea353ea
Abby Asomani <63776659+abbyabridged@users.noreply.github.com>
2020-10-06 00:28:11 +0800

    Fatalities viz in progress

---
+++ static/js/barchart3.js
@@ -0,0 +1,77 @@
+// Plotly.d3.csv('https://raw.githubusercontent.com/abbyabridged/wa-crash-dashboard/master/fatalities_data.csv', function(err, rows){
+
+Plotly.d3.csv('static/js/fatalities_data_count.csv', function(err, rows){
+
+function unpack(rows, key) {
+  return rows.map(function(row) {
+    return row[key];
+  });
+}
+
+var trace1 = {
+  x: unpack(rows, 'Dayweek'),
+  y: unpack(rows, 'Count'),
+  name: "Fatal crashes",
+  type: 'bar',
+  xaxis: 'x',
+  yaxis: 'y'
+};
+
+var trace2 = {
+  x: unpack(rows, 'Dayweek'),
+  y: unpack(rows, 'Number Fatalities'),
+  name: "Fatalities",
+  type: 'bar',
+  xaxis: 'x',
+  yaxis: 'y'
+};
+
+var data = [trace1, trace2];
+
+var layout = {
+  title: "Fatalities/fatal crashes by day of the week",
+  barmode: 'group'
+};
+
+
+Plotly.newPlot('barchart3', data, layout);
+});
+
+
+
+Plotly.d3.csv('static/js/fatalities_data_count.csv', function(err, rows){
+
+  function unpack(rows, key) {
+    return rows.map(function(row) {
+      return row[key];
+    });
+  }
+
+var trace3 = {
+  x: unpack(rows, 'Time of Day'),
+  y: unpack(rows, 'Count'),
+  name: "Fatal crashes",
+  type: 'bar',
+  xaxis: 'x',
+  yaxis: 'y'
+};
+
+var trace4 = {
+  x: unpack(rows, 'Time of Day'),
+  y: unpack(rows, 'Number Fatalities'),
+  name: "Fatalities",
+  type: 'bar',
+  xaxis: 'x',
+  yaxis: 'y'
+};
+
+var data = [trace3, trace4];
+
+var layout = {
+  title: "Fatalities/fatal crashes by time of day",
+  barmode: 'group'
+};
+
+
+Plotly.newPlot('barchart4', data, layout);
+});
